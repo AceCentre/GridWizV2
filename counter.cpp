783df2391669df4c2404946027d37dac8ce0a497
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-03-03 15:28:05 +0000

    Remove logs from counter

--- counter.cpp
+++ counter.cpp
@@ -1,17 +1,14 @@
 #include "counter.h"
-#include "qdebug.h"
 
 Counter::Counter(QObject *parent)
     : QObject{parent}
 {
-    qDebug("This is called?");
 
     this->m_value = 0;
 }
 
 void Counter::setValue(int value)
 {
-    qDebug() << "Setting Value" + QString::number(m_value) + "to" +  QString::number(value);
 
     if (value != m_value) {
         m_value = value;
@@ -21,7 +18,6 @@ void Counter::setValue(int value)
 
 void Counter::increment()
 {
-    qDebug() << "Incrementing";
     this->setValue(this->m_value + 1);
 }
 
